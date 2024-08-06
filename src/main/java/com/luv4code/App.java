361b14952ee2bab6f4176fc563711122a885b96d
promotheous-coder <madhav.p.akv@outlook.com>
2024-08-06 17:57:20 +0530

    initail commit

---
+++ src/main/java/com/luv4code/App.java
@@ -0,0 +1,13 @@
+package com.luv4code;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
